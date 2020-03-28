9e2695c6a1fe2a338a2cf9b84cbdfd9c58216f42
Syed Haider Raza <haideriu@hotmail.com>
2020-03-29 00:17:35 +0500

    firebase

---
+++ src/database/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyBN6cpy-TOgJ9mVZEpWtQXYhh2GnZjLwKk",
+    authDomain: "callingapp-2b74d.firebaseapp.com",
+    databaseURL: "https://callingapp-2b74d.firebaseio.com",
+    projectId: "callingapp-2b74d",
+    storageBucket: "callingapp-2b74d.appspot.com",
+    messagingSenderId: "227660605340",
+    appId: "1:227660605340:web:09f181af124c0908094a59",
+    measurementId: "G-F4PFRQZM23"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase;
---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Login from './login';
+import Signup from './signup';
+import Dashboard from './dashboard';
+
+export {
+    Login,
+    Signup,
+    Dashboard
+}
